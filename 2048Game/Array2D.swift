56d04e009b6abd35ff96863a96ebb36ab710d665
Ignacy Ślusarczyk <ignacy3@gmail.com>
2017-01-23 16:11:10 +0100

    first commit

---
+++ 2048Game/Array2D.swift
@@ -0,0 +1,31 @@
+//
+//  Array2D.swift
+//  2048Game
+//
+//  Created by airnauts on 23.01.2017.
+//  Copyright © 2017 ignacy. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    // #2
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        // #3
+        array = Array<T?>(repeating: nil, count:rows * columns)
+    }
+    
+    // #4
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
